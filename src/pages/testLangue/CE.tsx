f60569aeb1a435a1b02c0328aec096d257d10e72
LordMomonga <kamisisamuel28@gmail.com>
2024-10-21 09:42:11 +0100

    en cour

--- src/pages/testLangue/CE.tsx
+++ src/pages/testLangue/CE.tsx
@@ -234,11 +234,11 @@ const CE = () => {
 
   return (
     <div className='w-screen relative'>
-      <nav className=' px-5 py-2 flex items-center bg-prim '>
+      <nav className=' px-5 py-1 md:py-2 flex items-center bg-prim '>
         <span className=' font-bold text-3xl text-white'>Tolkin</span>
         </nav>
         
-       <div className='z-10 fixed left-0 px-2 bg-prim h-[85%]'>
+       <div className='hidden md:fixed z-10 left-0 px-2 bg-prim h-[80%] md:block'>
            <span className='mt-8 block font-bold  text-sm uppercase text-white '>comprehension Ecrite </span>
             <div className='mt-5 px-1 bg-white  py-2'>
               
@@ -252,35 +252,38 @@ const CE = () => {
                     <div className='mt-2 text-sm font-bold flex items-center gap-3 '><BiError className=' text-white bg-red-500 rounded-full font-bold  '></BiError><span>Aucune  :</span></div>
                 </div>
         </div>      
-        <div className=' z-10 fixed right-0 px-4 bg-prim h-[85%]'>
-              <div className='bg-white px-5  text-gray-600  text-sm text-center rounded-md py-2'>
+        <div className='z-10 fixed hidden md:block  right-0  px-1 md:px-4 bg-prim h-[80%]'>
+              <div className='bg-white md:px-5 px-1  text-gray-600   text-[10px] md:text-sm text-center rounded-md py-2'>
                 <h1 className='font-bold underline underline-offset-4 '>Mon profil</h1>
                 <div className=' py-3 text-left'>
                     <span className='font-bold'>Nom : <span className='text-prim font-bolder'>{user?.username}</span></span>
                     <span className='block mt-2 font-bold'>Adresse : <span className='text-prim font-bolder'>{user?.email}</span></span>
-                    <span className='block mt-2 font-bold'>Partie : Comprehension Ecrite</span>
-                    <span className='block mt-2 font-bold'>Durée : 60min</span>
+                    <span className='block mt-2 font-bold'>Partie : Comprehension Orale</span>
+                    <span className='block mt-2 font-bold'>Durée : 35min</span>
 
                 </div>
               </div>
+              <div>
               
-            <div className="bg-white text-gray-500 mt-[30%] mb-5 rounded-md px-5 py-2">
-                    <div className='text-[13px] font-bold flex items-center gap-3 '> <BiQuestionMark className=' text-white bg-blue-500 rounded-full font-bold  '></BiQuestionMark><span> Temps : {formatTime(remainingTime)} </span> </div>
-                    <div className='mt-2 text-[13px] font-bold flex items-center gap-3 '><BiAlarmExclamation className=' text-white bg-green-500 rounded-full font-bold  '></BiAlarmExclamation><span>Temps restant :</span></div>
+
+              </div>
+              
+            <div className="bg-white text-gray-500 mt-[30%] mb-5 rounded-md px-1  text-[10px] md:text-sm md:px-5 py-2">
+                    <div className=' text-[10px] md:text-sm font-bold flex items-center gap-3 '> <BiQuestionMark className=' text-white bg-blue-500 rounded-full font-bold  '></BiQuestionMark><span> Temps : {formatTime(remainingTime)} </span> </div>
+                    <div className='mt-2  text-[10px] md:text-sm font-bold flex items-center gap-3 '><BiAlarmExclamation className=' text-white bg-green-500 rounded-full font-bold  '></BiAlarmExclamation><span>Temps restant :</span></div>
                 </div>
-        </div>      
-        <div className='z-5 fixed bottom-0 bg-prim w-screen flex justify-between py-2 px-[10%] '>
-        <div className='bg-white text-gray-600 px-5 py-1 rounded-sm font-bold flex gap-2 items-center'><NavLink to='/Eorale' className='flex items-center gap-2'><BiSkipNext className='text-md bg-green-500 text-white'></BiSkipNext>skip this test</NavLink></div>
+        </div>
+        <div className='z-10 fixed bottom-0 bg-prim w-screen flex justify-between py-5 px-[10%] '>
         <div className='bg-white text-gray-600 px-5 py-1 rounded-sm font-bold flex gap-2 items-center '><span onClick={handleExit} className='flex items-center gap-2'><BiExit className='text-md bg-red-500 text-white'></BiExit>quit the examination</span></div>
         </div>
        
-        <div className='bg-white h-[80%] w-[68%] py-2 left-[14%] px-5 text-gray-700 fixed z-0'>
+        <div className='bg-white h-[80%] w-[100%] md:w-[68%] py-2 left-0 md:left-[13.5%] overflow-y-auto px-5 text-gray-700 fixed z-0'>
         <div className="text-sm font-bold text-center">
                 lisez le test et  Choisissez la bonne réponse en cochant sur la bonne reponse. 
             </div>
             <div className='w-full relative'>
                 <div className='flex justify-center'>
-                <img className=' w-[50%] z-0' onClick={handleImageToggle}  src={currentQuestion?.imageUrl} alt="" />
+                <img className=' w-[40%] z-0' onClick={handleImageToggle}  src={currentQuestion?.imageUrl} alt="" />
                
                 </div>
                 <div className='pl-[15%] justify-center '>
@@ -288,7 +291,7 @@ const CE = () => {
                 {currentIndex + 1}- {currentQuestion?.question}
                 </span>
                 <form action="">
-                 <div className='flex gap-5 mb-2'>
+                 <div className='flex gap-5'>
                  <input type="radio" name="question" value="1"
                   checked={selectedAnswer === "1"}
                  onChange={handleChange} id="option-a" />
